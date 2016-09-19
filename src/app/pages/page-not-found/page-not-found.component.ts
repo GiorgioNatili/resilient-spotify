97ebf618ef75b76c7d902ac22ba4ee57bbccc71b
GiorgioNatili <g.natili@gnstudio.com>
2016-09-19 08:35:05 -0400

    Fix message for potential 404

--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+    console.log("What??? Another 404?")
+  }
 
   ngOnInit() {
   }
