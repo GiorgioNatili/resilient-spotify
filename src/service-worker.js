7becaf6a168383e1b810da27206fcc347cdecf9d
GiorgioNatili <g.natili@gnstudio.com>
2016-10-12 17:57:07 -0400

    Fix cache cleaning

--- src/service-worker.js
+++ src/service-worker.js
@@ -24,6 +24,18 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
 
     console.log('activate ServiceWorker', event);
+
+    event.waitUntil(
+        caches.keys().then(function(keyList) {
+
+            return Promise.all(keyList.map(function(key) {
+                
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
